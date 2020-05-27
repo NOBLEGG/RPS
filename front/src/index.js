ba853676f2dd47a3663f1cb3f0e367f172f50136
NOBLEGG <bornnine3@hanmail.net>
2020-05-27 17:46:25 +0900

    Add packages : styled-components, cross-env

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'index.css';
+import BaseLayout from 'BaseLayout';
+import * as serviceWorker from 'serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BaseLayout />
   </React.StrictMode>,
   document.getElementById('root')
 );
