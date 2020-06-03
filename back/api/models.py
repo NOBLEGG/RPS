9834c53cf25e01ca3dae94e5d1c909622754e104
NOBLEGG <bornnine3@hanmail.net>
2020-06-03 15:27:18 +0900

    Uninstall app : mongoengine

--- back/api/models.py
+++
@@ -1,6 +0,0 @@
-from mongoengine import connect, DynamicDocument, StringField, DateTimeField
-
-####################################################################################################
-########################################### Keep private ###########################################
-####################################################################################################
-connect('rps', host='mongodb+srv://admin:imnotyourchild@test-drdx8.mongodb.net/test?retryWrites=true&w=majority')
