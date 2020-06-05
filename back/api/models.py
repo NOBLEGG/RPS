547430e21da3c0768f1c044c0695cf2555b0c044
NOBLEGG <bornnine3@hanmail.net>
2020-06-05 11:49:42 +0900

    ENFORCE_SCHEMA : True, Notice migration

---
+++ back/api/models.py
@@ -0,0 +1,8 @@
+from djongo import models
+
+class Notice(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    view = models.PositiveIntegerField()
